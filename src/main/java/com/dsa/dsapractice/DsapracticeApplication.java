34cbee0dbefc10aa9aa52c0c74f8e26b30599225
anindyachatterjee666 <anindyachatterjee666@gmail.com>
2025-01-08 17:06:15 +0530

    created test class

---
+++ src/main/java/com/dsa/dsapractice/DsapracticeApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.dsapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsapracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsapracticeApplication.class, args);
+	}
+
+}
